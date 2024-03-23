4c4f3a10f67ffe764447ad4b5c86242310ea6d8e
abdokin <bashbin.sh@gmail.com>
2024-03-23 18:23:04 +0000

    add main

--- src/main/java/com/abdokin/Main.java
+++ src/main/java/com/abdokin/Main.java
@@ -1,6 +1,17 @@
 package com.abdokin;
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello world");
+        if (args.length != 1) {
+            System.out.println("Usage: java Main <regex>");
+            return;
+        }
+        String regex = args[0];
+        NondeterministicAutomaton afn = new NondeterministicAutomaton();
+        AutomateBuilder builder = new AutomateBuilder(afn, new RegexParser(regex));
+        builder.build();
+        String fileName = "afn.dot";
+        afn.generateDOT(fileName);
+        AutomateMinimizer.minimize(afn);
+
     }
 }
